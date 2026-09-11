227df8c7be2dcbf621016f8e1bb5cd1967a0c4cd
koteswar singh <koteswar31204@gmail.com>
2026-09-11 11:36:12 +0530

    fix : Hero section UI

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,18 +9,19 @@ const Hero = () => {
 
             <div className='hero-content'>
                 <div className='hero-text'>
-                    <p className='name'>Hi I'm <span className='developer-name'>Koteswar Singh</span> </p>
-                    <h2>Frontend React Developer</h2>
-                    <p className='hero-description'>I build responsive and interactive web applications using React.js,
-                        JavaScript, HTML5 and CSS3. Passionate about creating clean user
-                        interfaces and integrating REST APIs.
+                    <p className='name'>
+                        Hi I'm <span className='developer-name'>Koteswar Singh</span>
+                    </p>
+                    <h2>MERN Stack Developer</h2>
+                    <p className='hero-description'>
+                        I build responsive and full-stack web applications using React.js, Node.js, Express.js, and MongoDB. Experienced in developing REST APIs, authentication, database integration, and modern web interfaces.
                     </p>
 
                     <div className='hero-buttons'>
                         <a href='#projects'>View Projects</a>
                         <a href='#contact'>Contact Me</a>
 
-                  </div>
+                    </div>
                 </div>
 
                 <div className='profile-photo'>
